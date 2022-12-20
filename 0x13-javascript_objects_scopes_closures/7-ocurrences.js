049c0102f6e2d7ffef13f527ab171c7677925727
daniel ngao muema <danielngao1999@gmail.com>
2022-12-20 15:17:16 -0800

    changes to 7-occurrences file

--- 0x13-javascript_objects_scopes_closures/7-ocurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-ocurrences.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
-// function that returns the number of occurrences in a list
 exports.nbOccurences = function (list, searchElement) {
-  let times = 0;
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] === searchElement) {
-      times += 1;
-    }
-  }
-  return times;
+  return  list.reduce((count, current) => (current === searchElement ? count + 1 : count ), 0);
 };
